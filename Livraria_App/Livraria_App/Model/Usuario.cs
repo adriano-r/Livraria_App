2e56ee9c0aba7fc9ccd8ce2f4115eced87365e39
adriano <adrianorosa9974@gmail.com>
2023-06-16 17:00:57 -0300

    Fix Register Page

--- Livraria_App/Livraria_App/Model/Usuario.cs
+++ Livraria_App/Livraria_App/Model/Usuario.cs
@@ -20,6 +20,7 @@ namespace Livraria_App.Model
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
         public string Email { get; set; }
+        public string Senha { get; set; }
         public string Avatar { get; set; }
     }
 
--- Livraria_App/Livraria_App/View/RegisterPage.xaml.cs
+++ Livraria_App/Livraria_App/View/RegisterPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Livraria_App.Model;
+using Livraria_App.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,20 +14,65 @@ namespace Livraria_App.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RegisterPage : ContentPage
     {
+        private Usuario usuario;
+        private UsuariosApi api;
+        private List<Usuario> usuarios;
         public RegisterPage()
         {
             InitializeComponent();
+            api = new UsuariosApi();
+            usuarios = new List<Usuario>();
+            LoadUsuarios();
         }
 
+        private async void LoadUsuarios()
+        {
+            try
+            {
+                usuarios = await api.GetUsuarios();
+            }
+            catch (Exception ex)
+            {
+               await DisplayAlert("Erro", ex.Message, "Ok");
+            }
+        }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
             Navigation.PushAsync(new LoginPage());
         }
 
-        private void Registrar_Clicked(object sender, EventArgs e)
+        private async void Registrar_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                usuario = new Usuario();
+                usuario.Nome = entNome.Text;
+                usuario.Sobrenome = entSobre.Text;
+                usuario.Email = entEmail.Text;
+                usuario.Senha = entSenha.Text;
+                usuario.Avatar = string.Empty;
 
+                    await api.CreateUsuario(usuario);
+                    await DisplayAlert("Alerta", "Usuário criado com sucesso!", "Ok");
+
+                LoadUsuarios();
+
+                this.LimpaCampos();
+            }
+            catch (Exception error)
+            {
+                await DisplayAlert("Erro", error.Message, "Ok");
+            }
+        }
+
+        private void LimpaCampos()
+        {
+            entNome.Text = string.Empty;
+            entSobre.Text = string.Empty;
+            entEmail.Text = string.Empty;
+            entSenha.Text = string.Empty;
+            //entAvt.Text = string.Empty;
         }
     }
 }
